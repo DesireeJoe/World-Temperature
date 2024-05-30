5218cb729873d258facd598ff7e66a9ab1dc7be9
Fiona <fionamurphy90@gmail.com>
2024-05-30 11:31:11 +0200

    Update stream.py
    Added text to intro page

--- stream.py
+++ stream.py
@@ -42,6 +42,20 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+if page == 'Introduction':
+   st.write("## World Temperature: Effects of Greenhouse Emissions on Gobal Temperatures")
+   st.markdown("""
+Understanding what impacts our planet's temperature changes over time is vital for understanding the dynamics of climate change. 
 
+The goal of our project is to analyze the relationship between rising greenhouse gas emissions and their effect on global temperatures. 
+
+
+This project dives into historical temperature records to uncover trends and patterns, using data from FAO, NASA and ‘Our World In Data”.
+
+We want to understand how global warming has evolved over centuries and decades. We'll start by carefully looking at temperature data, going from the past to the present. Through detailed analysis and visualisation, we'll reveal how temperatures have changed across different parts of the world and over time.
+
+We'll also explore what factors might be driving these changes, such as carbon dioxide emissions and regional climate patterns.
+
+    """)
 
 
