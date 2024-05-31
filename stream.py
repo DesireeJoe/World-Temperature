d635a0845051757bfcdc0c87835579678922d41f
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-05-31 17:03:45 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -354,8 +354,8 @@ if page ==  "Exploration Analysis - Surface Temperature Anomaly":
                      "- For an in-detail description see [Surface Temeprature Anomaly Data](https://ourworldindata.org/grapher/hadcrut-surface-temperature-anomaly)\n")
 
          st.markdown("***")
-         with st.expander("Missing values"):
-              st.markdown("#### Missing values")
+         
+         st.markdown("#### Missing values")
 
     # Total missing values
          mis_val = sta.isnull().sum()
