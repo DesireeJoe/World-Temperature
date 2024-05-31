1c809eb6331a21a521adcdba1ba73044128f5d77
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-05-31 15:51:32 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -263,27 +263,27 @@ if page == "Exploration Analysis - OWID":
          st.markdown("#### Missing values")
 
     # Create a function that calculates the percentage of missing values in each column of your dataset.
-         def missing_values_table(df):
+      def missing_values_table(df):
     # Total missing values
-             mis_val = df.isnull().sum()
+            mis_val = df.isnull().sum()
 
     # Percentage of missing values
-             mis_val_percent = 100 * mis_val / len(df)
+            mis_val_percent = 100 * mis_val / len(df)
 
     # Make a table with the results
-             mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+            mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
 
     # Rename the columns
-             mis_val_table_ren_columns = mis_val_table.rename(columns={0: 'Missing Values', 1: '% of Total Values'})
+            mis_val_table_ren_columns = mis_val_table.rename(columns={0: 'Missing Values', 1: '% of Total Values'})
 
     # Sort the table by percentage of missing descending
-             mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values('% of Total Values', ascending=True).round(1)
+            mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values('% of Total Values', ascending=True).round(1)
 
     # Return the dataframe with missing information
-             return mis_val_table_ren_columns
+            return mis_val_table_ren_columns
     
     # Calculate the missing values table
-             missing_table = missing_values_table(df_OWID)
+            missing_table = missing_values_table(df_OWID)
 
     # Display the missing values table using st.dataframe
              st.dataframe(missing_table)
