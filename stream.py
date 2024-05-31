566d6604cd217d468a4ac3132c6a37055ea103d6
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-05-31 16:47:20 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -262,19 +262,19 @@ if page == "Exploration Analysis - OWID":
          st.markdown("#### Missing values")
 
     # Total missing values
-    mis_val = Co2.isnull().sum()
+         mis_val = Co2.isnull().sum()
     
     # Percentage of missing values
-    mis_val_percent = 100 * mis_val / len(Co2)
+         mis_val_percent = 100 * mis_val / len(Co2)
     
     # Make a table with the results
-    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+         mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
     
     # Rename the columns
-    mis_val_table_ren_columns = mis_val_table.rename(columns={0: 'Missing Values', 1: '% of Total Values'})
+         mis_val_table_ren_columns = mis_val_table.rename(columns={0: 'Missing Values', 1: '% of Total Values'})
     
     # Sort the table by percentage of missing descending and filter out columns with no missing values
-    mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+         mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values('% of Total Values', ascending=False).round(1)
 
     # Display the missing values table using Streamlit
     st.title("CO2 Dataset Missing Values Analysis")
