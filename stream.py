0a3f757bb6f3c894e5bd74ffdc86f6be9ddcc44e
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-04 10:17:55 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -417,35 +417,37 @@ if page ==  "Exploration Analysis - OWID":
   """)
 
 #Line plot for Global Co2 emissions by emission sources 
-
+if page ==  "Exploration Analysis - OWID":
+   with st.expander("Line plot representing Global CO2 Emissions by Emission Sources"):
+        st.write("The line plot illustrates global CO2 emissions over time, categorized by various emission sources. Each line in the plot represents the trend of CO2 emissions from a specific source, such as flaring, industrial processes, methane, nitrous oxide, oil, gas, coal, cement production, land use changes, and the total greenhouse gas emissions.")
 # Convert the 'year' column to an integer
-  Co2['year'] = Co2['year'].astype(int)
+        Co2['year'] = Co2['year'].astype(int)
 
 # Filter data for years from 1880 onwards
-  Co2 = Co2[Co2['year'] >= 1880]
+        Co2 = Co2[Co2['year'] >= 1880]
 # Select relevant columns for CO2 emissions by different sources
-  emission_sources = ['flaring_co2', 'other_industry_co2', 'methane', 'nitrous_oxide',
+        emission_sources = ['flaring_co2', 'other_industry_co2', 'methane', 'nitrous_oxide',
                     'oil_co2', 'gas_co2', 'coal_co2', 'cement_co2', 'total_ghg', 'land_use_change_co2']
 
 # Aggregate data by summing over years
-  emission_data = Co2.groupby('year')[emission_sources].sum()
+        emission_data = Co2.groupby('year')[emission_sources].sum()
  
 # Mapping variable names to custom legend labels
-  legend_labels = {
-    'flaring_co2': 'Flaring CO2',
-    'other_industry_co2': 'Other Industry CO2',
-    'methane': 'Methane',
-    'nitrous_oxide': 'Nitrous Oxide',
-    'oil_co2': 'Oil CO2',
-    'gas_co2': 'Gas CO2',
-    'coal_co2': 'Coal CO2',
-    'cement_co2': 'Cement CO2',
-    'total_ghg': 'Total GHG',
-    'land_use_change_co2': 'Land Use Change CO2'
-     }
+        legend_labels = {
+       'flaring_co2': 'Flaring CO2',
+       'other_industry_co2': 'Other Industry CO2',
+       'methane': 'Methane',
+       'nitrous_oxide': 'Nitrous Oxide',
+       'oil_co2': 'Oil CO2',
+       'gas_co2': 'Gas CO2',
+       'coal_co2': 'Coal CO2',
+       'cement_co2': 'Cement CO2',
+       'total_ghg': 'Total GHG',
+       'land_use_change_co2': 'Land Use Change CO2'
+         }
 
 # Plotting
-  plt.figure(figsize=(12, 6))
+        plt.figure(figsize=(12, 6))
 
   for source in emission_sources:
       plt.plot(emission_data.index, emission_data[source], label=legend_labels[source])
@@ -459,8 +461,7 @@ if page ==  "Exploration Analysis - OWID":
 
 # Display the plot in Streamlit
       st.pyplot(plt)
-with st.expander("Line plot representing Global CO2 Emissions by Emission Sources"):
-     st.write("The line plot illustrates global CO2 emissions over time, categorized by various emission sources. Each line in the plot represents the trend of CO2 emissions from a specific source, such as flaring, industrial processes, methane, nitrous oxide, oil, gas, coal, cement production, land use changes, and the total greenhouse gas emissions.")
+
 # Description of the plot
 with st.expander("### Description of the Global CO2 Emissions by Emission Sources"):
        st.write("""
