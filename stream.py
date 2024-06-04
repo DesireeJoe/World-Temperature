3e854505463a28667c33efdf174f617ac7e532c6
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-04 10:33:30 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -659,7 +659,7 @@ with st.expander("Description of Surface Temperature Anomaly Trends"):
    - An interesting observation is the falling trend in surface temperature anomaly for Afghanistan around 2017, indicating a deviation from the overall increasing trend observed in other countries.
    - This anomaly might warrant further investigation into the factors influencing temperature patterns in Afghanistan.
     """)     
-       st.markdown("***")
+      st.markdown("***")
 
 if page ==  "Exploration Analysis - STA":
       sns.set_style("whitegrid")
@@ -671,44 +671,44 @@ if page ==  "Exploration Analysis - STA":
       merged_data = load_data()    
   
 # Title and Plot Title Description
-      st.markdown("<h2 style='text-align: center;'>CO2 Emissions and Surface Temperature Anomalies Over Years</h2>", unsafe_allow_html=True)
-      st.write("The Line plot represents two line plots on the same graph. The first line plot depicts the trend of surface temperature anomaly over the years from 1850 to 2017. The second line plot illustrates the trend of CO2 emissions over the years from 1880 to 2022.")
+with st.expander("CO2 Emissions and Surface Temperature Anomalies Over Years"):
+     st.write("The Line plot represents two line plots on the same graph. The first line plot depicts the trend of surface temperature anomaly over the years from 1850 to 2017. The second line plot illustrates the trend of CO2 emissions over the years from 1880 to 2022.")
 
 # Create a figure and axis object
-      fig, ax1 = plt.subplots(figsize=(12, 6))
+     fig, ax1 = plt.subplots(figsize=(12, 6))
 
 # Plot CO2 emissions on the primary y-axis
-      sns.lineplot(data=merged_data, x='Year', y='co2', color='red', ax=ax1, label='CO2 Emissions')
+     sns.lineplot(data=merged_data, x='Year', y='co2', color='red', ax=ax1, label='CO2 Emissions')
 
 # Set the y-label for CO2 emissions
-      ax1.set_ylabel('CO2 Emissions (Tonnes)', color='red')
+     ax1.set_ylabel('CO2 Emissions (Tonnes)', color='red')
  
 # Create a secondary y-axis for Surface Temperature Anomaly
-      ax2 = ax1.twinx()
-      sns.lineplot(data=merged_data, x='Year', y='Surface temperature anomaly', color='blue', ax=ax2, label='Surface Temperature Anomaly')
+     ax2 = ax1.twinx()
+     sns.lineplot(data=merged_data, x='Year', y='Surface temperature anomaly', color='blue', ax=ax2, label='Surface Temperature Anomaly')
 
 # Set the y-label for Surface Temperature Anomaly
-      ax2.set_ylabel('Surface Temperature Anomaly (°C)', color='blue')
+     ax2.set_ylabel('Surface Temperature Anomaly (°C)', color='blue')
 
 # Set labels and title
-      ax1.set_xlabel('Year')
-      plt.title('CO2 Emissions and Surface Temperature Anomaly Over Years')
+     ax1.set_xlabel('Year')
+     plt.title('CO2 Emissions and Surface Temperature Anomaly Over Years')
 
 # Show legend
-      lines1, labels1 = ax1.get_legend_handles_labels()
-      lines2, labels2 = ax2.get_legend_handles_labels()
-      ax1.legend(lines1, ['CO2 Emissions'], loc='upper left')
-      ax2.legend(lines2, ['Surface Temperature Anomaly'], loc='upper right') 
+     lines1, labels1 = ax1.get_legend_handles_labels()
+     lines2, labels2 = ax2.get_legend_handles_labels()
+     ax1.legend(lines1, ['CO2 Emissions'], loc='upper left')
+     ax2.legend(lines2, ['Surface Temperature Anomaly'], loc='upper right') 
 
 # Rotate x-axis labels for better readability
-      plt.xticks(rotation=45)
+     plt.xticks(rotation=45)
 
 # Show the plot
-      st.pyplot(fig)
+     st.pyplot(fig)
 
 # Description of the plot
-      st.markdown("### Description of CO2 Emissions and Surface Temperature Anomalies Trends")
-      st.write("""                                                                                                                                  
+with st.expander("Description of CO2 Emissions and Surface Temperature Anomalies Trends"):
+     st.write("""                                                                                                                                  
     - Both line plots show an overall increasing trend over the respective time periods.
     - The surface temperature anomaly exhibits a steady increase from 1850 to 2017, while CO2 emissions show a rising trend from 1880 to 2022.
     - Despite the general upward trajectory, both plots also exhibit periods of fluctuations and variability.
@@ -725,12 +725,12 @@ if page ==  "Exploration Analysis - STA":
     import plotly.express as px
     import pandas as pd
 
-    st.markdown("<h2 style='text-align: center;'>Surface Temperature Anomalies Over Years in different countries</h2>", unsafe_allow_html=True)
-    st.write("The plot shows surface temperature anomaly over the years from 1850 to 2017 across different countries")
+with st.expander("Surface Temperature Anomalies Over Years in different countries"):
+     st.write("The plot shows surface temperature anomaly over the years from 1850 to 2017 across different countries")
     # Sort the values of Year Column
-    sta = sta.sort_values(by='Year')
+     sta = sta.sort_values(by='Year')
     # Plotly Choropleth Map with a different color scale
-    fig = px.choropleth(
+     fig = px.choropleth(
           sta,
           locations='Code',
           color='Surface temperature anomaly',
@@ -739,23 +739,23 @@ if page ==  "Exploration Analysis - STA":
           projection='natural earth',
           title='Surface Temperature Anomaly Over Time',
           color_continuous_scale='Viridis'  # Change the color scale to Viridis
-     )
+      )
 
     # Customize the layout
-    fig.update_layout(
-        coloraxis_colorbar=dict(
-        title='Surface Temperature Anomaly (°C)'
-    ),
-    coloraxis_colorbar_thickness=25,
-    coloraxis_colorbar_len=0.5,
-    autosize=False,
-    width=1000,
-    height=600,
-    xaxis=dict(range=[1850, 2017])
-    )
+      fig.update_layout(
+          coloraxis_colorbar=dict(
+          title='Surface Temperature Anomaly (°C)'
+       ),
+      coloraxis_colorbar_thickness=25,
+      coloraxis_colorbar_len=0.5,
+      autosize=False,
+      width=1000,
+      height=600,
+      xaxis=dict(range=[1850, 2017])
+      )
 
 # Display the map in Streamlit
-    st.plotly_chart(fig)
+      st.plotly_chart(fig)
 ########################################################################################################################################################################################################################
 
 if page ==  "Modelling Preparation":
