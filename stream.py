93c63d8d658b2773ad8efbb29c1421b381417b0e
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-04 09:56:12 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -464,7 +464,7 @@ if page ==  "Exploration Analysis - OWID":
   st.pyplot(plt)
 
 # Description of the plot
-  with st.expander("### Description of the CO2 Emissions Distribution"):
+  with st.expander("### Description of the Global CO2 Emissions by Emission Sources"):
        st.write("""
   - The contributions of different emission sources to the total CO2 emissions vary over time.
   - Some sources might show increasing trends, while others may exhibit fluctuations or decreasing patterns.
