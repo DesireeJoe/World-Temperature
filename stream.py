7bd19670a176f8be26f97f4afeff8e8ff803a191
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-04 09:53:49 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -327,17 +327,17 @@ if page == "Exploration Analysis - OWID":
 
     # Display the missing values table using Streamlit
     
-           st.dataframe(mis_val_table_ren_columns)
+         st.dataframe(mis_val_table_ren_columns)
     
-           st.write('**Having a more in detail look at the amount of missing values in the data set shows that:**')
-           st.markdown('  * There is a large amount of missing values in the data set, accumulating to 56,62% of all values in the data set.')
-           st.markdown('  * The amount of missing values varies a great deal across variables,')
-           st.markdown('  * Some variables have a comparably low percentage of missing values and are below 1/3 of all entries (e.g. share_global_luc_co2, co2),')
-           st.markdown('  * While others with amount of missing values exceed 90% of entries (e.g. consumption_co2, other_industry_co2)')
+         st.write('**Having a more in detail look at the amount of missing values in the data set shows that:**')
+         st.markdown('  * There is a large amount of missing values in the data set, accumulating to 56,62% of all values in the data set.')
+         st.markdown('  * The amount of missing values varies a great deal across variables,')
+         st.markdown('  * Some variables have a comparably low percentage of missing values and are below 1/3 of all entries (e.g. share_global_luc_co2, co2),')
+         st.markdown('  * While others with amount of missing values exceed 90% of entries (e.g. consumption_co2, other_industry_co2)')
     
-           st.markdown('  * The high share of missing values across a large part of the variables in the OWID data set (ranging from 15.3% to 94.9% across variables) poses some challenges to data selection and data preprocessing that might influence interpretability of the results further down the road.')
+         st.markdown('  * The high share of missing values across a large part of the variables in the OWID data set (ranging from 15.3% to 94.9% across variables) poses some challenges to data selection and data preprocessing that might influence interpretability of the results further down the road.')
 
-           st.markdown("***")
+         st.markdown("***")
 if page ==  "Exploration Analysis - OWID":
 #Plots
 #Barplot of different categories of C02 emissions
