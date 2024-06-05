badc6a77cf6e800404de3e03b498ed17c6bdc47b
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-05 12:07:03 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -672,18 +672,18 @@ if page ==  "Exploration Analysis - STA":
 
 if page ==  "Exploration Analysis - STA":
       sns.set_style("whitegrid")
-  
+
       @st.cache
       def load_data():
-        merged_data = pd.read_csv("merged_data.csv", encoding='latin1')
-        return merged_data
-      merged_data = load_data()  
-  
+          merged_data = pd.read_csv("merged_data.csv", encoding='latin1')
+          return merged_data
+       merged_data = load_data()  
 # Title and Plot Title Description
 if page ==  "Exploration Analysis - STA":
  with st.expander("CO2 Emissions and Surface Temperature Anomalies Over Years"):
       st.write("The Line plot represents two line plots on the same graph. The first line plot depicts the trend of surface temperature anomaly over the years from 1850 to 2017. The second line plot illustrates the trend of CO2 emissions over the years from 1880 to 2022.")
-   
+
+      # Create a figure and axis object
       fig, ax1 = plt.subplots(figsize=(12, 6))
       # Plot CO2 emissions on the primary y-axis
       sns.lineplot(data=merged_data, x='Year', y='co2', color='red', ax=ax1, label='CO2 Emissions')
