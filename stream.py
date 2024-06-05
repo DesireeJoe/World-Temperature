fbfdf38bca1a8f7cf60ed874d9bc5b4c4eda17b5
manasideshpande2209 <112894212+manasideshpande2209@users.noreply.github.com>
2024-06-05 12:14:34 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -679,7 +679,7 @@ if page ==  "Exploration Analysis - STA":
           return merged_data
       merged_data = load_data()  
   
- with st.expander("CO2 Emissions and Surface Temperature Anomalies Over Years"):
+with st.expander("CO2 Emissions and Surface Temperature Anomalies Over Years"):
       st.write("The Line plot represents two line plots on the same graph. The first line plot depicts the trend of surface temperature anomaly over the years from 1850 to 2017. The second line plot illustrates the trend of CO2 emissions over the years from 1880 to 2022.")
 
       # Create a figure and axis object
