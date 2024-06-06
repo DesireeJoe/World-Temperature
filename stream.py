9e274123ba838ee003b702ebb46efceded1de3cb
DesireeJoe <168346313+DesireeJoe@users.noreply.github.com>
2024-06-06 13:14:06 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1499,8 +1499,8 @@ if page == "Exploration Analysis - FAO":
         ETC_cleaned = pd.read_csv('ETC.csv', encoding='latin1')
         return ETC_all, ETC_all_noflag, ETC_all_area_codes, ETC_all_area_flags, ETC_cleaned
 
-    ETC_all, ETC_all_noflag, ETC_all_area_codes, ETC_all_area_flags, ETC_cleaned = load_data()
-        st.markdown("""
+  ETC_all, ETC_all_noflag, ETC_all_area_codes, ETC_all_area_flags, ETC_cleaned = load_data()
+  st.markdown("""
         The FAOSTAT Temperature Change on land domain provides comprehensive statistics on mean surface temperature changes by country from 1961 to 2019, with updates on a yearly basis. This initial step of data exploration serves as a first step to our broader goal to visualize and comprehend the intricate dynamics driving climate change. 
 
         Before we explored more datasets and analyzing the greenhouse gases we first wanted to know: are global temperatures really increasing? Hence, our decision to explore this dataset stems from a fundamental concern: understanding the profound impact of greenhouse gases on our planet's climate. A more detailed description of the dataset can be found in the dropdown below. 
