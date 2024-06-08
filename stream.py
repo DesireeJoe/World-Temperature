0baf67bffa5c4b820b1aa78a7d0bb09d84c79b12
DesireeJoe <168346313+DesireeJoe@users.noreply.github.com>
2024-06-08 14:52:06 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -173,12 +173,12 @@ if page ==  "🔬 Exploration Analysis - NASA":
 # Load data
 @st.cache
 def load_data():
-nasa = pd.read_csv("NASA_zonal.csv", encoding='latin1')
-# Remove commas and convert 'Year' column to integers
-# Remove commas and convert 'Year' column to integers
-nasa['Year'] = nasa['Year'].apply(lambda x: int(x.replace(',', '')) if isinstance(x, str) else x)
-
-
+    nasa = pd.read_csv("NASA_zonal.csv", encoding='latin1')
+    # Remove commas and convert 'Year' column to integers
+    # Remove commas and convert 'Year' column to integers
+    nasa['Year'] = nasa['Year'].apply(lambda x: int(x.replace(',', '')) if isinstance(x, str) else x)
+    
+    
 return nasa
 
 nasa = load_data()
